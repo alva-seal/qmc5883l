29a65e903e3db0333158214998c70c16c7d7f625
ffabi <github@ffabi.com>
2018-01-28 12:00:28 +0100

    Create __init__.py

---
+++ qmc5883l/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+"""Driver for the QMC5883l 3-Axis Magnetic Sensor
+Datasheet: https://github.com/e-Gizmo/QMC5883L-GY-271-Compass-module/blob/master/QMC5883L%20Datasheet%201.0%20.pdf
+"""
+
+__author__ = "Fabian Mruck"
+__email__ = "github@ffabi.com"
+__license__ = 'MIT'
+__version__ = 'v0.1'
+
+"""HISTORY
+v.01 - First 
