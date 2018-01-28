a035383083fb2f76218bbe57b587d390c0db4ac9
ffabi <github@ffabi.com>
2018-01-28 11:58:48 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qmc5883l',
+      version='0.1',
+      description='Driver for qmc5883l 3-Axis Magnetic Sensor',
+      url='http://github.com/norges/qmc5883l',
+      author='Fabian Mruck',
+      author_email='github@ffabi.com',
+      license='MIT',
+      packages=['qmc5883l'],
+      zip_safe=False)
