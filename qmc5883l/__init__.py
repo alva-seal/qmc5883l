a884ebd0494c58daa27d56729a6e4ea24253e3f8
ffabi <github@ffabi.com>
2018-02-01 14:17:11 +0000

    Update __init__.py

--- qmc5883l/__init__.py
+++ qmc5883l/__init__.py
@@ -95,3 +95,11 @@ class QMC5883L(object):
         self.cntrl_reg2 = self.cntrl_reg2 + self.restore * (2 ** 7)
         self.bus.write_byte_data(self.adress, REG_CONF_1, self.cntrl_reg1)
         self.bus.write_byte_data(self.adress, REG_CONF_2, self.cntrl_reg2)
+        
+    def get_temp(self):
+        return self.bus.read_byte_data(self.adress, REG_TEMP_MSB) * 2.56 + self.bus.read_byte_data(self.adress, REG_TEMP_LSB) / 100
+    
+    def get_magnet(self):
+        self.read_byte_data(self.adress, REG_OUT_X_LSB)
+        return [0 0 0]
+        
