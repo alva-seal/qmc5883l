849cee1b91291655d04f845facc507d2becee275
Fabian <github@ffabi.com>
2018-02-11 11:40:56 +0100

    testing is mor complicatet than I thougth SMBus has to bee mocked will be done later

--- tests/test_qmc5883l.py
+++ tests/test_qmc5883l.py
@@ -1,11 +1,19 @@
 import pytest
+import
 
 import qmc5883l
 
-
+"""
 def test_convert_data():
     data = [128, 128]
     offset = 0
     a = qmc5883l._convert_data(data, offset)
     b = qmc5883l.qmc5883l.QMC5883L._convert_data(data, offset)
     print(a, b)
+"""
+
+
+def test_init__empty_params__():
+    magnet_sensor = qmc5883l.QMC5883L()
+    assert magnet_sensor.busnum is 1
+    ^
