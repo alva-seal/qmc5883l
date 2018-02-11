54d12866efa741c249875758d756b112901beea1
Fabian <github@ffabi.com>
2018-02-11 12:09:06 +0100

    ready for first comit to pypi

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # qmc5883l
-Python driver for qmc5883l 3-Axis Magnetic Sensor
+Python driver for qmc5883l 3-Axis Magnetic Sensor on Raspberry pi
+
+You can get the temperature and teh magnet values out of a qmc5883l
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='qmc5883l',
-      version='0.1',
+      version='0.92',
       description='Driver for qmc5883l 3-Axis Magnetic Sensor',
       url='http://github.com/norges/qmc5883l',
+      download_url='http://github.com/norges/qmc5883l/archive/0.92.tar.gz)',
       author='Fabian Mruck',
       author_email='github@ffabi.com',
       license='MIT',
